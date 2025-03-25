22ba53c027d638501bb27fa8238d6f4a1b9a3163
ppparkta <pppppark2@gmail.com>
2025-03-25 21:39:34 +0900

    feat: 데이터 중심 객체 구현

--- chapter_4/src/main/java/org/example/domain/Customer.java
+++ chapter_4/src/main/java/org/example/domain/Customer.java
@@ -1,4 +1,11 @@
 package org.example.domain;
 
 public class Customer {
+    private String name;
+    private String id;
+
+    public Customer(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
 }
