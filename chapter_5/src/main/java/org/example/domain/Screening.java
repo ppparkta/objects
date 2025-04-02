82dceb78d8fef6e42211ae53816963a09c69225e
ppparkta <pppppark2@gmail.com>
2025-04-03 02:47:27 +0900

    feat: 5장 책임 할당하기 첫번째 설계

---
+++ chapter_5/src/main/java/org/example/domain/Screening.java
@@ -0,0 +1,25 @@
+package org.example.domain;
+
+import java.time.LocalDateTime;
+
+public class Screening {
+    private LocalDateTime whenScreened;
+    private int sequence;
+    private Movie movie;
+
+    public Reservation reserve(Customer customer, int audienceCount) {
+        return new Reservation(customer, this, calculateFee(audienceCount), audienceCount);
+    }
+
+    private Money calculateFee(int audienceCount) {
+        return movie.calculateMovieFee(this).times(audienceCount);
+    }
+
+    public LocalDateTime getWhenScreened() {
+        return whenScreened;
+    }
+
+    public int getSequence() {
+        return sequence;
+    }
+}
---
+++ chapter_5/src/main/java/org/example/domain/discount/DiscountConditionType.java
@@ -0,0 +1,7 @@
+package org.example.domain.discount;
+
+public enum DiscountConditionType {
+    SEQUENCE,   // 순번 조건
+    PERIOD,     // 기간 조건
+    ;
+}
