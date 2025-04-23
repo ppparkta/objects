83bb05f6cbc0264c57f965e3549c72f850a3f9ed
ppparkta <pppppark2@gmail.com>
2025-04-24 01:22:46 +0900

    feat: 명령과 쿼리가 혼재하는 isSatisfied 메서드

--- chapter_6/src/main/java/org/example/Main.java
+++ chapter_6/src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        RecurringSchedule schedule = new RecurringSchedule("회의", DayOfWeek.THURSDAY,
+                LocalTime.of(10, 30), Duration.ofMinutes(30));
+
+        Event meeting1 = new Event("회의",
+                LocalDateTime.of(2025, 4, 24, 10, 30, 0),
+                Duration.ofMinutes(30));
+
+        Event meeting2 = new Event("회의",
+                LocalDateTime.of(2025, 4, 25, 10, 30, 0),
+                Duration.ofMinutes(30));
+
+        assert meeting2.isSatisfied(schedule) == false;
+        assert meeting2.isSatisfied(schedule) == true;
+
+
     }
 }
